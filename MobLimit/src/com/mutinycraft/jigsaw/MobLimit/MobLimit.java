5e07564f5f083781cecf07a8f47d67a8038385ae
Jigsaw <admin@mutinycraft.com>
2013-01-10 19:59:13 -0600

    It might help to check if the spawn event was from a spawner.

--- MobLimit/src/com/mutinycraft/jigsaw/MobLimit/MobLimit.java
+++ MobLimit/src/com/mutinycraft/jigsaw/MobLimit/MobLimit.java
@@ -6,33 +6,39 @@ import org.bukkit.entity.EntityType;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.CreatureSpawnEvent;
+import org.bukkit.event.entity.CreatureSpawnEvent.SpawnReason;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class MobLimit extends JavaPlugin implements Listener{
-	
+public class MobLimit extends JavaPlugin implements Listener {
+
 	Logger log;
 	private static final String VERSION = "1.0";
-	
+
 	private static final EntityType CAVE_SPIDER = EntityType.CAVE_SPIDER;
 	private static final EntityType SKELETON = EntityType.SKELETON;
 	private static final EntityType SPIDER = EntityType.SPIDER;
 	private static final EntityType ZOMBIE = EntityType.ZOMBIE;
-	
-	public void onEnable(){
+
+	@Override
+	public void onEnable() {
 		log = this.getLogger();
 		getServer().getPluginManager().registerEvents(this, this);
 		log.info(this.getName() + " v" + VERSION + " enabled!");
 	}
-	
+
 	@EventHandler
-	public void SpawnerEvent(CreatureSpawnEvent e){
+	public void SpawnerEvent(CreatureSpawnEvent e) {
 		EntityType type = e.getEntityType();
-		if(type == CAVE_SPIDER || type == SKELETON || type == SPIDER || type == ZOMBIE ){
-			e.setCancelled(true);
+		if (e.getSpawnReason() == SpawnReason.SPAWNER) {
+			if (type == CAVE_SPIDER || type == SKELETON || type == SPIDER
+					|| type == ZOMBIE) {
+				e.setCancelled(true);
+			}
 		}
 	}
-	
-	public void onDisable(){
+
+	@Override
+	public void onDisable() {
 		log.info(this.getName() + " v" + VERSION + " disabled!");
 	}
 
